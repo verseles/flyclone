c291f33b7cd4d34c0fc75be17c3b8f282ad3fc32
Hélio <insign@gmail.com>
2025-06-10 03:14:49 -0300

    fix(ProcessTimedOutException): Include timeout type and value in message

--- src/Exception/ProcessTimedOutException.php
+++ src/Exception/ProcessTimedOutException.php
@@ -1,13 +1,26 @@
 <?php
 
-
 namespace Verseles\Flyclone\Exception;
 
+use Symfony\Component\Process\Exception\ProcessTimedOutException as SymfonyProcessTimedOutException;
 
 class ProcessTimedOutException extends RcloneException
 {
-   public function __construct(\Exception $exception, string $message = 'The process took more than defined.', int $code = 22)
-   {
-      parent::__construct($message, $code, $exception);
-   }
+  /**
+   * @param SymfonyProcessTimedOutException $exception The original exception from Symfony Process.
+   * @param int                             $code      The exception code.
+   */
+  public function __construct(SymfonyProcessTimedOutException $exception, int $code = 22)
+  {
+    $message = 'The process timed out.'; // Default message
+    
+    // Determine which timeout was exceeded by inspecting the Symfony exception object.
+    if ($exception->isIdleTimeout()) {
+      $message = sprintf('The process exceeded the idle timeout of %s seconds.', $exception->getIdleTimeout());
+    } elseif ($exception->isGeneralTimeout()) {
+      $message = sprintf('The process exceeded the total timeout of %s seconds.', $exception->getTimeout());
+    }
+    
+    parent::__construct($message, $code, $exception);
+  }
 }
